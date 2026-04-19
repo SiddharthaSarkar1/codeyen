a2b2ca12dbbc41aa30859ca226b62f6b0f165ddd
sarkartukai25 <sarkartukai25@gmail.com>
2026-04-19 20:46:07 +0000

    my projects - page structure

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import type { Project } from '../types';
+import { Loader2Icon, PlusIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const MyProjects = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const navigate = useNavigate()
+
+  const fetchProjects = async () => {
+
+    //Simulate project loading
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+
+  }
+
+  useEffect(() => {
+    fetchProjects()
+  }, [])
+
   return (
-    <div>MyProjects</div>
+    <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
+      {
+        loading ? (
+          <div className="flex items-center justify-center h-[80vh]">
+            <Loader2Icon className="size-7 animate-spin text-indigo-200" />
+          </div>
+        ) : projects.length > 0 ? (
+          <div className='py-10 min-h-[80vh]'>
+            <div className="flex items-center justify-between mb-12">
+              <h1 className='text-2xl font-medium text-white'>My Projects</h1>
+              <button onClick={() => navigate('/')} className='flex items-center gap-2 text-white px-3 sm:px-6 py-1 sm:py-2 rounded bg-linear-to-br from-indigo-500 to-indigo-600 hover:from-indigo-600 hover:to-indigo-500 hover:opacity-90 active:scale-95 transition-all'><PlusIcon size={18} /> Create New</button>
+            </div>
+          </div>
+        ) : (
+          <div className='flex flex-col items-center justify-center h-[80vh] gap-3'>
+            <h1 className='text-2xl font-medium text-white'>No Projects Found.</h1>
+            <button onClick={() => navigate('/')} className='text-white px-3 sm:px-6 py-1 sm:py-2 rounded bg-linear-to-br from-indigo-500 to-indigo-600 hover:from-indigo-600 hover:to-indigo-500 hover:opacity-90 active:scale-95 transition-all'>Create New</button>
+          </div>
+        )
+      }
+
+    </div>
   )
 }
 
