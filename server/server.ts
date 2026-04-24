07d67544666e3dc4be21af17cbde974c472796de
sarkartukai25 <sarkartukai25@gmail.com>
2026-04-24 21:40:38 +0000

    add project controller and route

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import userRouter from "./routes/userRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 dotenv.config();
 
@@ -17,13 +19,20 @@ const corsOptions = {
 
 //Middleware
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.get("/healthz", (req: Request, res: Response) => {
+    res.json({ status: "OK"})
+})
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ status: "OK", message: "Server is live now." });
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/project", projectRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
